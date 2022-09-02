2fb08cec95cade1d5c3a56dd5d43d90f95efecf0
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-09-02 15:59:04 +0100

    attempted extensions

--- safari_park_lab.sql
+++ safari_park_lab.sql
@@ -12,7 +12,7 @@ CREATE TABLE assignments (id SERIAL PRIMARY KEY, employee_Id int, enclosure_Id i
 
 -- Content
 INSERT INTO animals (name, type, age, enclosure_Id) values ('Tony', 'Tiger', 59, 1);
-INSERT INTO animals (name, type, age, enclosure_Id) values ('Tanya', 'Tiger', 35, 1);
+INSERT INTO animals (name, type, age, enclosure_Id) values ('Tanya', 'Tiger', 59, 1);
 INSERT INTO animals (name, type, age, enclosure_Id) values ('Stripey', 'Zebra', 17, 2);
 INSERT INTO animals (name, type, age, enclosure_Id) values ('C6', 'Otter', 28, 3);
 
@@ -42,3 +42,37 @@ inner join assignments
 on staffs.id = assignments.employee_id
 inner join enclosures
 on assignments.enclosure_id = enclosures.id;
+
+-- Extensions
+-- The names of staff working in enclosures which are closed for maintenance
+select staffs.name as staff, enclosures.name as enclosure_name
+from staffs
+inner join assignments
+on staffs.id = assignments.employee_id
+inner join enclosures
+on assignments.enclosure_id = enclosures.id
+where enclosures.closed_for_maintenance = 'true';
+
+-- The name of the enclosure where the oldest animal lives. If there are two animals who are the same age choose the first one alphabetically.
+select enclosures.name as enclosure_name, animals.name as animal, animals.age as age
+from animals
+inner join enclosures
+on animals.enclosure_id = enclosures.id
+order by animals.age desc, animals.name asc
+limit 1;
+
+-- The number of different animal types a given keeper has been assigned to work with.
+-- ran out of time to add staff names
+select count (distinct animals.type) name
+from staffs
+inner join assignments
+on staffs.id = assignments.employee_id
+inner join enclosures
+on enclosures.id = assignments.enclosure_id
+inner join animals
+on enclosures.id = animals.enclosure_id
+group by staffs.name;
+
+-- The number of different keepers who have been assigned to work in a given enclosure
+
+-- The names of the other animals sharing an enclosure with a given animal (eg. find the names of all the animals sharing the big cat field with Tony)
