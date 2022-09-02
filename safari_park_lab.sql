5b811759bd0dc7a0144d1caac4df2796a189791e
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-09-02 15:13:44 +0100

    completed mvp

--- safari_park_lab.sql
+++ safari_park_lab.sql
@@ -12,6 +12,7 @@ CREATE TABLE assignments (id SERIAL PRIMARY KEY, employee_Id int, enclosure_Id i
 
 -- Content
 INSERT INTO animals (name, type, age, enclosure_Id) values ('Tony', 'Tiger', 59, 1);
+INSERT INTO animals (name, type, age, enclosure_Id) values ('Tanya', 'Tiger', 35, 1);
 INSERT INTO animals (name, type, age, enclosure_Id) values ('Stripey', 'Zebra', 17, 2);
 INSERT INTO animals (name, type, age, enclosure_Id) values ('C6', 'Otter', 28, 3);
 
@@ -26,4 +27,18 @@ INSERT INTO assignments (employee_Id, enclosure_Id, day) values (1, 1, 'Tuesday'
 INSERT INTO assignments (employee_Id, enclosure_Id, day) values (1, 2, 'Tuesday');
 INSERT INTO assignments (employee_Id, enclosure_Id, day) values (2, 3, 'Wednesday');
 
+-- Solutions
+-- MVP
+-- The names of the animals in a given enclosure
+select animals.name, enclosures.name
+from animals
+inner join enclosures
+on animals.enclosure_id = enclosures.id;
 
+-- The names of the staff working in a given enclosure
+select staffs.name as staff, enclosures.name as enclosure_name
+from staffs
+inner join assignments
+on staffs.id = assignments.employee_id
+inner join enclosures
+on assignments.enclosure_id = enclosures.id;
